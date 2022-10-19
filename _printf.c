8c94e2f6b48a78b34face89729b79cfb8b6db334
Samuel Iyeh <thesamueliyeh@gmail.com>
2022-10-19 01:29:09 -0700

    Another commit

--- _printf.c
+++ _printf.c
@@ -29,19 +29,28 @@ int _printf(const char *format, ...)
 			switch (format[i + 1])
 			{
 				case 'c':
-					ch = va_arg(list, int);
-					write(1, &ch, sizeof(ch));
-					i++, strlen += sizeof(ch); break;
+					{
+						ch = va_arg(list, int);
+						write(1, &ch, sizeof(ch));
+						i++, strlen += sizeof(ch);
+						break;
+					}
 				case 's':
-					string = va_arg(list, char *);
-					for (u = 0; string[u] != '\0'; u++)
-						;
-					write(1, string, u);
-					i++, strlen += u; break;
+					{
+						string = va_arg(list, char *);
+						for (u = 0; string[u] != '\0'; u++)
+							;
+						write(1, string, u);
+						i++, strlen += u;
+						break;
+					}
 				case '%':
-					percent = '%';
-					write(1, &percent, sizeof(percent));
-					i++, strlen += sizeof(percent); break;
+					{
+						percent = '%';
+						write(1, &percent, sizeof(percent));
+						i++, strlen += sizeof(percent);
+						break;
+					}
 			}
 		}
 		else
